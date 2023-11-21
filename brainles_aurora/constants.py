3693d78971123455361b9b8b0a00943c67916b7c
Marcel Rosier <marcel.rosier@gmx.de>
2023-11-21 16:00:06 +0100

    - fixed inconsistent naming

--- brainles_aurora/constants.py
+++ brainles_aurora/constants.py
@@ -3,8 +3,8 @@ from enum import Enum, auto
 
 class ModalityMode(str, Enum):
     T1_T1C_T2_FLA = 't1-t1c-t2-fla'
-    T1C_T1_FLA = 't1c-t1-fla'
-    T1C_T1 = 't1c-t1'
+    T1_T1C_FLA = 't1-t1c-fla'
+    T1_T1C = 't1-t1c'
     T1C_FLA = 't1c-fla'
     T1C_O = 't1c-o'
     FLA_O = 'fla-o'
