1be82647a88c533ea7198b3721aa40d6158934bd
Marcel Rosier <marcel.rosier@gmx.de>
2023-11-23 15:25:39 +0100

    - add primiotive unittest setup

---
+++ tests/test_inferer.py
@@ -0,0 +1,46 @@
+# Call 'python -m unittest' on this folder
+# coverage run -m unittest
+# coverage report
+# coverage html
+import unittest
+
+import nibabel as nib
+import numpy as np
+import logging
+from brainles_aurora.inferer import CPUInferer
+
+
+class TestInferer(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.t1 = "example_data/BraTS-MET-00110-000-t1n.nii.gz"
+        self.t1c = "example_data/BraTS-MET-00110-000-t1c.nii.gz"
+        self.t2 = "example_data/BraTS-MET-00110-000-t2w.nii.gz"
+        self.fla = "example_data/BraTS-MET-00110-000-t2f.nii.gz"
+
+        return super().setUp()
+
+    def load_np_from_nifti(self, path: str) -> np.ndarray:
+        return nib.load(path).get_fdata()
+
+    def test_invalid_inference_mode(self):
+        """Might change with new models, rather a dummy test"""
+        with self.assertRaises(NotImplementedError):
+            inferer = CPUInferer(segmentation_file="your_segmentation_file.nii.gz",
+                                 #    t1=self.t1,
+                                 t1c=self.t1c,
+                                 t2=self.t2,
+                                 fla=self.fla)
+            inferer.infer()
+
+    def test_mixed_input_types(self):
+        with self.assertRaises(AssertionError):
+            inferer = CPUInferer(segmentation_file="your_segmentation_file.nii.gz",
+                                 t1=self.t1,
+                                 t1c=self.load_np_from_nifti(self.t1c),
+                                 )
+            inferer.infer()
+
+    def tearDown(self) -> None:
+        logging.shutdown()
+        return super().tearDown()
