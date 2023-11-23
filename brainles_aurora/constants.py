b31fbe628938247110389a13d0aa193e7e08a3fa
Marcel Rosier <marcel.rosier@gmx.de>
2023-11-23 13:58:43 +0100

    - add input modes and rename to inference mode

--- brainles_aurora/constants.py
+++ brainles_aurora/constants.py
@@ -1,7 +1,7 @@
-from enum import Enum, auto
+from enum import Enum
 
 
-class ModalityMode(str, Enum):
+class InferenceMode(str, Enum):
     T1_T1C_T2_FLA = 't1-t1c-t2-fla'
     T1_T1C_FLA = 't1-t1c-fla'
     T1_T1C = 't1-t1c'
@@ -17,13 +17,18 @@ class ModelSelection(str, Enum):
     VANILLA = 'vanilla'
 
 
+class InputMode(str, Enum):
+    FILE = "NIFTI_FILEPATH"
+    NUMPY = "NP_NDARRAY"
+
+
 # booleans indicate presence of files in order: T1 T1C T2 FLAIR
-FILES_TO_MODE_DICT = {
-    (True, True, True, True): ModalityMode.T1_T1C_T2_FLA,
-    (True, True, False, True): ModalityMode.T1_T1C_FLA,
-    (True, True, False, False): ModalityMode.T1_T1C,
-    (False, True, False, True): ModalityMode.T1C_FLA,
-    (False, True, False, False): ModalityMode.T1C_O,
-    (False, False, False, True): ModalityMode.FLA_O,
-    (True, False, False, False): ModalityMode.T1_O,
+IMGS_TO_MODE_DICT = {
+    (True, True, True, True): InferenceMode.T1_T1C_T2_FLA,
+    (True, True, False, True): InferenceMode.T1_T1C_FLA,
+    (True, True, False, False): InferenceMode.T1_T1C,
+    (False, True, False, True): InferenceMode.T1C_FLA,
+    (False, True, False, False): InferenceMode.T1C_O,
+    (False, False, False, True): InferenceMode.FLA_O,
+    (True, False, False, False): InferenceMode.T1_O,
 }
