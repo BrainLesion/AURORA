55de6d99d33c81c3af2b5cbea9fd79ce5b9ee371
Marcel Rosier <marcel.rosier@tum.de>
2024-01-16 18:46:26 +0100

    - adding and refactoring of constants/ dataclasses

--- brainles_aurora/inferer/constants.py
+++ brainles_aurora/inferer/constants.py
@@ -2,7 +2,17 @@ from enum import Enum
 
 
 class InferenceMode(str, Enum):
-    """Enum representing different modes of inference based on available image inputs."""
+    """Enum representing different modes of inference based on available image inputs
+
+    Enum Values:
+        T1_T1C_T2_FLA (str): All four modalities are available.
+        T1_T1C_FLA (str): T1, T1C, and FLAIR are available.
+        T1_T1C (str): T1 and T1C are available.
+        T1C_FLA (str): T1C and FLAIR are available.
+        T1C_O (str): T1C is available.
+        FLA_O (str): FLAIR is available.
+        T1_O (str): T1 is available.
+    """
 
     T1_T1C_T2_FLA = "t1-t1c-t2-fla"
     T1_T1C_FLA = "t1-t1c-fla"
@@ -14,7 +24,13 @@ class InferenceMode(str, Enum):
 
 
 class ModelSelection(str, Enum):
-    """Enum representing different strategies for model selection."""
+    """Enum representing different strategies for model selection.
+
+    Enum Values:
+        BEST (str): Select the best performing model.
+        LAST (str): Select the last model.
+        VANILLA (str): Select the vanilla model.
+    """
 
     BEST = "best"
     LAST = "last"
@@ -33,6 +49,24 @@ class DataMode(str, Enum):
     NUMPY = "NP_NDARRAY"
 
 
+class Output(str, Enum):
+    """Enum representing different types of output.
+
+    Enum Values:
+        SEGMENTATION (str): Segmentation mask.
+        WHOLE_NETWORK (str): Whole network output.
+        METASTASIS_NETWORK (str): Metastasis network output.
+    """
+
+    SEGMENTATION = "segmentation"
+    WHOLE_NETWORK = "whole_network"
+    METASTASIS_NETWORK = "metastasis_network"
+
+
+MODALITIES = ["t1", "t1c", "t2", "fla"]
+"""List of modality names in standard order: T1 T1C T2 FLAIR (['t1', 't1c', 't2', 'fla'])"""
+
+
 # booleans indicate presence of files in order: T1 T1C T2 FLAIR
 IMGS_TO_MODE_DICT = {
     (True, True, True, True): InferenceMode.T1_T1C_T2_FLA,
@@ -43,3 +77,4 @@ IMGS_TO_MODE_DICT = {
     (False, False, False, True): InferenceMode.FLA_O,
     (True, False, False, False): InferenceMode.T1_O,
 }
+"""Dictionary mapping tuples of booleans representing presence of the modality in order [t1,t1c,t2,fla] to InferenceMode values."""
--- brainles_aurora/inferer/dataclasses.py
+++ brainles_aurora/inferer/dataclasses.py
@@ -1,9 +1,7 @@
 import logging
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Tuple
 
-import numpy as np
 
 from brainles_aurora.inferer.constants import DataMode, ModelSelection
 
@@ -14,23 +12,11 @@ class BaseConfig:
 
     Attributes:
         output_mode (DataMode, optional): Output mode for the inference results. Defaults to DataMode.NIFTI_FILE.
-        output_folder (str | Path, optional): Output folder for the results. Defaults to "aurora_output".
         log_level (int | str, optional): Logging level. Defaults to logging.INFO.
-        segmentation_file_name (str, optional): File name for the segmentation result. Defaults to "segmentation.nii.gz". (The segmentation will be saved in output_folder/{timestamp}/segmentation_file_name)
-        t1 (str | Path | np.ndarray | None, optional): Path or NumPy array for T1 image. Defaults to None.
-        t1c (str | Path | np.ndarray | None, optional): Path or NumPy array for T1 contrast-enhanced image. Defaults to None.
-        t2 (str | Path | np.ndarray | None, optional): Path or NumPy array for T2 image. Defaults to None.
-        fla (str | Path | np.ndarray | None, optional): Path or NumPy array for FLAIR image. Defaults to None.
     """
 
     output_mode: DataMode = DataMode.NIFTI_FILE
-    output_folder: str | Path = "aurora_output"
-    segmentation_file_name: str | None = "segmentation.nii.gz"
     log_level: int | str = logging.INFO
-    t1: str | Path | np.ndarray | None = None
-    t1c: str | Path | np.ndarray | None = None
-    t2: str | Path | np.ndarray | None = None
-    fla: str | Path | np.ndarray | None = None
 
 
 @dataclass
@@ -39,15 +25,7 @@ class AuroraInfererConfig(BaseConfig):
 
     Attributes:
         output_mode (DataMode, optional): Output mode for the inference results. Defaults to DataMode.NIFTI_FILE.
-        output_folder (str | Path, optional): Output folder for the results. Defaults to "aurora_output".
-        segmentation_file_name (str, optional): File name for the segmentation result. Defaults to "segmentation.nii.gz". (The segmentation will be saved in output_folder/{timestamp}/segmentation_file_name)
         log_level (int | str, optional): Logging level. Defaults to logging.INFO.
-        t1 (str | Path | np.ndarray | None, optional): Path or NumPy array for T1 image. Defaults to None.
-        t1c (str | Path | np.ndarray | None, optional): Path or NumPy array for T1 contrast-enhanced image. Defaults to None.
-        t2 (str | Path | np.ndarray | None, optional): Path or NumPy array for T2 image. Defaults to None.
-        fla (str | Path | np.ndarray | None, optional): Path or NumPy array for FLAIR image. Defaults to None.
-        output_whole_network (bool, optional): Whether to output the whole network results. Defaults to False.
-        output_metastasis_network (bool, optional): Whether to output the metastasis network results. Defaults to False.
         tta (bool, optional): Whether to apply test-time augmentations. Defaults to True.
         sliding_window_batch_size (int, optional): Batch size for sliding window inference. Defaults to 1.
         workers (int, optional): Number of workers for data loading. Defaults to 0.
@@ -55,10 +33,10 @@ class AuroraInfererConfig(BaseConfig):
         sliding_window_overlap (float, optional): Overlap ratio for sliding window inference. Defaults to 0.5.
         crop_size (Tuple[int, int, int], optional): Crop size for sliding window inference. Defaults to (192, 192, 32).
         model_selection (ModelSelection, optional): Model selection strategy. Defaults to ModelSelection.BEST.
+        include_whole_network_in_numpy_output_mode (bool, optional): Whether to include the whole network in numpy output mode. Defaults to False.
+        include_metastasis_network_in_numpy_output_mode (bool, optional): Whether to include the metastasis network in numpy output mode. Defaults to False.
     """
 
-    output_whole_network: bool = False
-    output_metastasis_network: bool = False
     tta: bool = True
     sliding_window_batch_size: int = 1
     workers: int = 0
@@ -66,3 +44,5 @@ class AuroraInfererConfig(BaseConfig):
     sliding_window_overlap: float = 0.5
     crop_size: Tuple[int, int, int] = (192, 192, 32)
     model_selection: ModelSelection = ModelSelection.BEST
+    include_whole_network_in_numpy_output_mode: bool = False
+    include_metastasis_network_in_numpy_output_mode: bool = False
