fac48a0649dd9ff94424b3f2bf8d2cdb54e82490
Marcel Rosier <marcel.rosier@tum.de>
2024-01-23 15:47:14 +0100

    - fixed testname, simplified

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,14 +4,13 @@ from brainles_aurora.utils import remove_path_suffixes
 
 
 class TestAuroraUtils:
-    def test_remove_path_suffixes_single_suffix(self):
+    def test_remove_path_suffixes(self):
         suffixes = [".nii", ".nii.gz", ".what.is.this.nii.gz", ""]
         path_stem = "path/to/test"
 
         paths_str = [path_stem + suffix for suffix in suffixes]
-        paths_path = [Path(path) for path in paths_str]
 
         assert all(
-            Path(path_stem) == remove_path_suffixes(p1) == remove_path_suffixes(p2)
-            for p1, p2 in zip(paths_path, paths_str)
+            Path(path_stem) == remove_path_suffixes(p) == remove_path_suffixes(Path(p))
+            for p in paths_str
         )
