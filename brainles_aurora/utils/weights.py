cb547981f99af49ff43cfa728a5f8ad44055fc64
Marcel Rosier <marcel.rosier@gmx.de>
2024-06-25 15:43:45 +0200

    fixed weights update logic fixed deprecated warning

--- brainles_aurora/utils/weights.py
+++ brainles_aurora/utils/weights.py
@@ -9,11 +9,12 @@ from pathlib import Path
 from typing import Dict, List
 
 import requests
+from tqdm import tqdm
 from brainles_aurora.inferer.constants import WEIGHTS_DIR_PATTERN
 
 logger = logging.getLogger(__name__)
 
-ZENODO_RECORD_URL = "https://zenodo.org/api/records/10557069"
+ZENODO_RECORD_URL = "https://zenodo.org/api/records/10557068"
 
 
 def check_model_weights() -> Path:
@@ -24,7 +25,7 @@ def check_model_weights() -> Path:
     """
     package_folder = Path(__file__).parent.parent
 
-    zenodo_metadata = _get_zenodo_metadata()
+    zenodo_metadata, archive_url = _get_zenodo_metadata_and_archive_url()
 
     matching_folders = list(package_folder.glob(WEIGHTS_DIR_PATTERN))
     # Get the latest downloaded weights
@@ -41,7 +42,9 @@ def check_model_weights() -> Path:
         )
 
         return _download_model_weights(
-            package_folder=package_folder, zenodo_metadata=zenodo_metadata
+            package_folder=package_folder,
+            zenodo_metadata=zenodo_metadata,
+            archive_url=archive_url,
         )
 
     logger.info(f"Found downloaded local weights: {latest_downloaded_weights}")
@@ -63,14 +66,19 @@ def check_model_weights() -> Path:
         f"New model weights available on Zenodo ({zenodo_metadata['version']}). Deleting old and fetching new weights..."
     )
     # delete old weights
-    shutil.rmtree(
-        package_folder / latest_downloaded_weights,
-        onerror=lambda func, path, excinfo: logger.warning(
-            f"Failed to delete {path}: {excinfo}"
+    try:
+        shutil.rmtree(
+            package_folder / latest_downloaded_weights,
+        )
+    except Exception as e:
+        logger.warning(
+            f"Failed to delete {package_folder / latest_downloaded_weights}: {e}"
         ),
-    )
+
     return _download_model_weights(
-        package_folder=package_folder, zenodo_metadata=zenodo_metadata
+        package_folder=package_folder,
+        zenodo_metadata=zenodo_metadata,
+        archive_url=archive_url,
     )
 
 
@@ -96,26 +104,33 @@ def _get_latest_version_folder_name(folders: List[Path]) -> str | None:
     return latest_downloaded_folder.name
 
 
-def _get_zenodo_metadata() -> Dict | None:
-    """Get the metadata for the Zenodo record.
+def _get_zenodo_metadata_and_archive_url() -> Dict | None:
+    """Get the metadata for the Zenodo record and the files archive url.
 
     Returns:
-        dict: Metadata for the Zenodo record.
+        Tuple: (dict: Metadata for the Zenodo record, str: URL to the archive file)
     """
     try:
         response = requests.get(ZENODO_RECORD_URL)
-        return response.json()["metadata"]
+        if response.status_code != 200:
+            logger.error(f"Cant find model weights on Zenodo. Exiting...")
+        data = response.json()
+        return data["metadata"], data["links"]["archive"]
+
     except requests.exceptions.RequestException as e:
         logger.warning(f"Failed to fetch Zenodo metadata: {e}")
         return None
 
 
-def _download_model_weights(package_folder: Path, zenodo_metadata: Dict) -> Path:
+def _download_model_weights(
+    package_folder: Path, zenodo_metadata: Dict, archive_url: str
+) -> Path:
     """Download the latest model weights from Zenodo and extract them to the target folder.
 
     Args:
         package_folder (Path): Package folder path in which the model weights will be stored.
         zenodo_metadata (Dict): Metadata for the Zenodo record.
+        archive_url (str): URL to the model weights archive file.
 
     Returns:
         Path: Path to the model weights folder.
@@ -129,15 +144,27 @@ def _download_model_weights(package_folder: Path, zenodo_metadata: Dict) -> Path
         f"Downloading model weights from Zenodo ({ZENODO_RECORD_URL}). This might take a while..."
     )
     # Make a GET request to the URL
-    response = requests.get(f"{ZENODO_RECORD_URL}/files-archive")
+    response = requests.get(archive_url, stream=True)
     # Ensure the request was successful
     if response.status_code != 200:
         logger.error(
             f"Failed to download model weights. Status code: {response.status_code}"
         )
         return
+        # Download with progress bar
+    chunk_size = 1024  # 1KB
+    bytes_io = BytesIO()
+    with tqdm(
+        total=0,  # unknown size since content length not given
+        unit="B",
+        unit_scale=True,
+    ) as pbar:
+        for data in response.iter_content(chunk_size=chunk_size):
+            bytes_io.write(data)
+            pbar.update(len(data))
+
     # Extract the downloaded zip file to the target folder
-    with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
+    with zipfile.ZipFile(bytes_io) as zip_ref:
         zip_ref.extractall(weights_folder)
     logger.info(f"Zip file extracted successfully to {weights_folder}")
     return weights_folder
